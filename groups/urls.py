aae5043ae75280d2a1732afcd31e0b4a4473d45a
Milosz Skaza <milosz.skaza@gmail.com>
2017-07-15 21:45:08 +0200

    Change GroupList URL and add GroupIndex.

--- groups/urls.py
+++ groups/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'groups'
 
 urlpatterns = [
-    url(r'^$/', views.ListGroupView.as_view(), name='list'),
+    url(r'^$', views.GroupListView.as_view(), name='list'),
     url(r'^new/$', views.CreateGroupView.as_view(), name='create'),
     url(r'^join/(?P<slug>[-\w]+)/$', views.JoinGroupView.as_view(), name='join'),
     url(r'^leave/(?P<slug>[-\w]+)/$', views.LeaveGroupView.as_view(), name='leave'),
+    url(r'^(?P<slug>[-\w]+)/$', views.GroupIndexView.as_view(), name='index'),
 ]
