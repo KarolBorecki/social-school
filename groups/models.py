86576c8212817d273e15936ce093500d31b7b3d1
Milosz Skaza <milosz.skaza@gmail.com>
2017-07-15 20:47:44 +0200

    Update and register models.

--- groups/models.py
+++ groups/models.py
@@ -3,16 +3,19 @@ from django.utils.text import slugify
 from django.contrib.auth.models import User
 # Create your models here.
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 class Group(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    slug = models.SlugField(allow_unicode=True, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True, blank=True)
     description = models.TextField(blank=True, default='')
     members = models.ManyToManyField(User, through='GroupMember')
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(models.Model, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
