dbddfdef768dd620e4be7bf34cf913d91c2b3e2b
Karol Borecki <karol.boreck@gmail.com>
2017-07-16 23:08:46 +0200

    Added email confirmations

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,9 @@ from accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
-    url(r'^/', views.IndexView.as_view(), name='index'),
-    url(r'^login/', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^logout/', auth_views.logout, {'template_name': 'accounts/index.html'}, name='logout'),
-    url(r'^register/', views.RegisterView.as_view(), name='register'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'accounts/index.html'}, name='logout'),
+    url(r'^active/(?P<user_id>[0-9]+)$', views.email_activation_view, name='email activation'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic import TemplateView, CreateView
+
 
 from accounts.forms import UserRegistrationForm
 
@@ -24,10 +27,20 @@ class RegisterView(CreateView):
 
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
+            user_email = form.cleaned_data['email']
             user.set_password(password)
+            user.is_active = False
             user.save()
 
             user = authenticate(username=username, password=password)
+
+            send_mail(
+                'Social School - Email confirmation ',
+                'MSG',
+                'our.email@shit.eu',
+                [user_email],
+                fail_silently=False,
+            )
 
             if user is not None:
                 if user.is_active:
@@ -35,3 +48,11 @@ class RegisterView(CreateView):
                     return redirect('accounts:index')
         else:
             return render(request, self.template_name, {'form': form})
+
+
+def email_activation_view(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.is_active = True
+    user.save()
+
+    return render(request, 'accounts/email_activation_successful.html')
