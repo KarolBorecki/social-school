81b4fd708f73b86e3d51e90bbd9954a2cd89b615
Karol Borecki <karol.boreck@gmail.com>
2017-07-16 23:35:43 +0200

    added email activation function and optimaze imports

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
-from django.views.generic import TemplateView, CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView
 
 from accounts.forms import UserRegistrationForm, LoginForm
 
@@ -35,6 +36,14 @@ class RegisterView(CreateView):
                     return redirect('index')
         else:
             return render(request, self.template_name, {'form': form})
+
+
+def email_activation_view(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.is_active = True
+    user.save()
+
+    return render(request, 'accounts/email_activation_successful.html')
 
 
 class LoginView(TemplateView):
