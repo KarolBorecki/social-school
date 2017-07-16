92c1b9902fd0d63920180f25e0517a768aa0f7f2
Karol Borecki <karol.boreck@gmail.com>
2017-07-16 23:31:46 +0200

    Added email activation system and LOGIN_REDIRECT_URL in settings.py set to '/'

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
-from django.views.generic import TemplateView, CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView
 
 from accounts.forms import UserRegistrationForm, LoginForm
 
@@ -24,17 +26,34 @@ class RegisterView(CreateView):
 
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
+            user_email = form.cleaned_data['email']
             user.set_password(password)
+            user.is_active = False
             user.save()
 
             user = authenticate(username=username, password=password)
 
+            send_mail(
+               'Social School - Email confirmation ',
+                'MSG',
+                'our.email@shit.eu',
+                [user_email],
+                fail_silently=False,
+               )
             if user is not None:
                 if user.is_active:
                     login(request, user)
                     return redirect('index')
         else:
             return render(request, self.template_name, {'form': form})
+
+
+def email_activation_view(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.is_active = True
+    user.save()
+
+    return render(request, 'accounts/email_activation_successful.html')
 
 
 class LoginView(TemplateView):
